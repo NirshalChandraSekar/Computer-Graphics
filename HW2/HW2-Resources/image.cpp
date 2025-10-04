4c875b86dec1b44a8fd23ef4491a3cdd083edaa7
NirshalChandraSekar <nirshalniru@gmail.com>
2025-10-04 18:03:40 -0500

    hw2 completed

--- HW2/HW2-Resources/image.cpp
+++ HW2/HW2-Resources/image.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
+#include <cmath>
 
 #include <fstream>
 using namespace std;
@@ -31,7 +32,7 @@ void write_ppm(char* imgName, int width, int height, int bits, const uint8_t *da
 
    //Set's the 3rd line to 255 (ie., assumes this is an 8 bit/pixel PPM)
    //TODO - HW2: Set the maximum values based on the value of the variable 'bits'
-   int maximum = 255;
+   int maximum = (int)pow(2, bits) - 1; // 2^bits - 1
    ppmFile << maximum << "\n" ;
 
    //TODO - HW2: The values in data are all 8 bits, you must convert down to whatever the variable bits is when writing the file
@@ -41,7 +42,10 @@ void write_ppm(char* imgName, int width, int height, int bits, const uint8_t *da
 		  r = data[i*width*4 + j*4 + 0];  //Red
         g = data[i*width*4 + j*4 + 1];  //Green
         b = data[i*width*4 + j*4 + 2];  //Blue
-        a = data[i*width*4 + j*4 + 3];  //Alpha
+
+        r = (round)((r / 255.0) * maximum);
+        g = (round)((g / 255.0) * maximum);
+        b = (round)((b / 255.0) * maximum);
         ppmFile << r << " " << g << " "  << b << " " ;
       }
    }
@@ -75,29 +79,37 @@ uint8_t* read_ppm(char* imgName, int& width, int& height){
    //Check that the 3rd line is 255 (ie., this is an 8 bit/pixel PPM)
    int maximum;
    ppmFile >> maximum;
-   //TODO - HW2: Remove this check below, instead make the function for for maximum values besides 255
-   if (maximum != 255) {
-      printf("ERROR: We assume Maximum size is 255, not (%d)\n",maximum);
-      printf("TODO: This error means you didn't finish your HW yet!\n");
-      exit(1);
-   }
 	
    //TODO - HW2: The values read from the file might not be 8-bits (ie, a maximum values besides 255)
    //TODO - HW2: However img_data stores all values as 8-bit integers.
    //TODO - HW2: When you read the values into img_data scale the values up to be 8 bits
    //TODO - HW2: For example, the value 1 in a 1 bit PPM should become 255 ... 
    //TODO - HW2: Likewise, the value 1 in a 2 bit PPM should become 127 (or 128).
+
    int r, g, b;
-   for (int i = 0; i < height; i++){
-      for (int j = 0; j < width; j++){
+   float bucket_size = 256.0 / (maximum + 1); // +1 to avoid division by zero
+   for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
          ppmFile >> r >> g >> b;
-         img_data[i*width*4 + j*4] = r;  //Red
-         img_data[i*width*4 + j*4 + 1] = g;  //Green
-         img_data[i*width*4 + j*4 + 2] = b;  //Blue
-         img_data[i*width*4 + j*4 + 3] = 255;  //Alpha
+
+         // Scale from [0, maximum] → [0, 255] using "middle of bucket"
+         // middle = (value * bucket_size) + (bucket_size / 2) - 1
+         int r_scaled = (int)(r * bucket_size + bucket_size / 2.0f - 1);
+         int g_scaled = (int)(g * bucket_size + bucket_size / 2.0f - 1);
+         int b_scaled = (int)(b * bucket_size + bucket_size / 2.0f - 1);
+
+         // Clamp to valid 0–255 range
+         r_scaled = std::max(0, std::min(255, r_scaled));
+         g_scaled = std::max(0, std::min(255, g_scaled));
+         b_scaled = std::max(0, std::min(255, b_scaled));
+
+         img_data[i*width*4 + j*4 + 0] = (uint8_t)r_scaled;
+         img_data[i*width*4 + j*4 + 1] = (uint8_t)g_scaled;
+         img_data[i*width*4 + j*4 + 2] = (uint8_t)b_scaled;
+         img_data[i*width*4 + j*4 + 3] = 255;  // Alpha always 255
       }
    }
-
+   ppmFile.close();
    return img_data;
 }
 
@@ -206,17 +218,63 @@ void Image::Brighten (double factor){
 
 //TODO - HW2: Crop an image to a rectangle starting at (x,y) with a width w and a height h
 Image* Image::Crop(int x, int y, int w, int h) {
-	/* WORK HERE */
-	return NULL;
+	// Make sure the crop area is inside the image
+    if (x < 0 || y < 0 || x + w > width || y + h > height) {
+        printf("ERROR: Crop region is out of image bounds!\n");
+        return nullptr;
+    }
+
+    // Create a new image of size w × h
+    Image* cropped = new Image(w, h);
+
+    // Copy pixels from original image into new image
+    for (int j = 0; j < h; j++) {
+        for (int i = 0; i < w; i++) {
+            // Compute source pixel position
+            Pixel p = GetPixel(x + i, y + j);
+
+            // Assign pixel to cropped image
+            cropped->GetPixel(i, j) = p;
+        }
+    }
+
+    return cropped;
 }
 
 //TODO - HW2: Keep only non-zero red, green, or blue components for the channel value 0, 1, and 2 respectively
 void Image::ExtractChannel(int channel) {
-	/* WORK HERE */
+	if (channel < 0 || channel > 2) {
+        printf("ERROR: Invalid channel %d! Must be 0 (red), 1 (green), or 2 (blue).\n", channel);
+        return;
+    }
+
+    for (int y = 0; y < Height(); y++) {
+        for (int x = 0; x < Width(); x++) {
+            Pixel& p = GetPixel(x, y);
+            if (channel == 0) { // Red
+                p.g = 0;
+                p.b = 0;
+            } else if (channel == 1) { // Green
+                p.r = 0;
+                p.b = 0;
+            } else if (channel == 2) { // Blue
+                p.r = 0;
+                p.g = 0;
+            }
+        }
+    }
 }
 
 //TODO - HW2: Quantize the intensities stored for each pixel's values into 2^nbits possible equally-spaced values
 //TODO - HW2: You may find a very helpful function in the pixel class!
 void Image::Quantize (int nbits) {
-	/* WORK HERE */
+	assert(nbits >= 1 && nbits <= 8); // validate bits
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            Pixel p = GetPixel(i, j);
+            Pixel q = PixelQuant(p, nbits);  // quantize using PixelQuant
+            GetPixel(i, j) = q;              // assign back to image
+        }
+    }
 }
